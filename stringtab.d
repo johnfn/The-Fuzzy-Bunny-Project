f27b37399cec1241844b0d71e91267be51662233
Grant Alexander Mathews <grantm@myth13.Stanford.EDU>
2010-11-13 15:13:27 -0800

    go away *.d

--- stringtab.d
+++
@@ -1,8 +0,0 @@
-stringtab.o stringtab.d : stringtab.cc \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/copyright.h \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/stringtab_functions.h \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/copyright.h \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/cool-io.h \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/stringtab.h \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/list.h \
-  /afs/ir/class/cs143/cool/fall10/include/PA5/stringtab.h
